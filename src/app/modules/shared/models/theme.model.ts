a5274db0bc74c15db0adeca2db1080b96706c118
EOussama <oussama_gtacj@live.fr>
2020-06-23 21:56:59 +0100

    Added more fields to the theme

--- src/app/modules/shared/models/theme.model.ts
+++ src/app/modules/shared/models/theme.model.ts
@@ -1,10 +1,42 @@
 export interface Theme {
+
+  /**
+   * The name of the theme
+   */
   name: string;
+
+  /**
+   * The link to the theme
+   */
   link: string;
+
+  /**
+   * The episode(s) the theme is used in
+   */
   episodes: string[];
-  tags: string[];
-  type: string;
+
+  /**
+   * The media format
+   */
+  format: string;
+
+  /**
+   * Whether the theme contains spoilers
+   */
   spoiler: boolean;
+
+  /**
+   * Whether the theme is NSFW
+   */
   nsfw: boolean;
+
+  /**
+   * Whether the theme has lyrics
+   */
   lyrics: boolean;
+
+  /**
+   * Whether the video is 1080p
+   */
+  HD: boolean;
 }
